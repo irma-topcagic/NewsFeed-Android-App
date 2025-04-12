36422278ab15381e8bf942180cecca94e3360f40
itopcagic1 <itopcagic1@etf.unsa.ba>
2025-04-12 15:15:19 +0200

    Commit

--- app/src/main/java/ba/etfrma/newsfeedapp/screen/NewsFeedScreen.kt
+++ app/src/main/java/ba/etfrma/newsfeedapp/screen/NewsFeedScreen.kt
@@ -2,28 +2,21 @@ package ba.etfrma.newsfeedapp.screen
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import ba.etfrma.newsfeedapp.data.getAllNews
-import ba.etfrma.newsfeedapp.model.NewsItem
+
 
 @SuppressLint("RememberReturnType")
 @Composable
 fun NewsFeedScreen() {
     val allNews = remember { getAllNews() }
-    var selectedCategory by remember { mutableStateOf("All") }
+    var selectedCategory by remember { mutableStateOf("Sve") }
 
     val filteredNews = remember(selectedCategory) {
-        if (selectedCategory == "All") allNews
+        if (selectedCategory == "Sve") allNews
         else allNews.filter { it.category == selectedCategory }
     }
 
@@ -34,11 +27,10 @@ fun NewsFeedScreen() {
     }
 
     Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
-        FilterChips(
+        FilterSection(
             selectedCategory = selectedCategory,
             onCategorySelected = { selectedCategory = it }
         )
-
         Spacer(modifier = Modifier.height(16.dp))
 
         if (filteredNews.isEmpty()) {
@@ -49,61 +41,7 @@ fun NewsFeedScreen() {
     }
 }
 
-@OptIn(ExperimentalLayoutApi::class)
-@Composable
-fun FilterChips(
-    selectedCategory: String,
-    onCategorySelected: (String) -> Unit
-) {
-    val categories = listOf("All", "Politika", "Sport", "Nauka/tehnologija", "Kultura")
 
-    FlowRow(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        categories.forEach { category ->
-            FilterChip(
-                selected = selectedCategory == category,
-                onClick = { if (selectedCategory != category) onCategorySelected(category) },
-                label = {
-                    Text(
-                        text = category,
-                        style = TextStyle(
-                            fontSize = if (category in listOf("All", "Politika", "Sport", "Nauka/tehnologija","Kultura" +
-                                        "")) 20.sp else 14.sp
-                        )
-                    )
-                },
-                modifier = Modifier.testTag(getChipTag(category))
-            )
-        }
-    }
-}
 
-fun getChipTag(category: String): String {
-    return when (category) {
-        "Politika" -> "filter_chip_pol"
-        "Sport" -> "filter_chip_spo"
-        "Nauka/tehnologija" -> "filter_chip_sci"
-        "All" -> "filter_chip_all"
-        else -> "filter_chip_unknown"
-    }
-}
 
-@Composable
-fun NewsList(newsItems: List<NewsItem>, listState: LazyListState) {
-    LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        state = listState
-    ) {
-        items(newsItems, key = { it.id }) { item ->
-            if (item.isFeatured) {
-                FeaturedNewsCard(newsItem = item)
-            } else {
-                StandardNewsCard(newsItem = item)
-            }
-        }
-    }
-}
+
